2511d33a61621f9d34f3f4ab8f7f55b3ca7a3b71
Cassio <cassioate@gmail.com>
2022-08-24 22:34:25 -0300

    refactor: change the logic of the movement the hero from components to moveHeroContext

--- src/components/molecules/buttons/arrows/index.tsx
+++ src/components/molecules/buttons/arrows/index.tsx
@@ -1,119 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { HeroMoveContext } from '../../../../context/moveHeroContext';
+import React from 'react';
 import { ArrowDown } from './arrow-down';
 import { ArrowLeft } from './arrow-left';
 import { ArrowRight } from './arrow-right';
 import { ArrowUp } from './arrow-up';
 import { SpaceKeyboard } from './space';
-import useEventListener from '@use-it/event-listener';
-import { Container, ContainerArrow } from './styles';
-import { HERO_SIZE_HEIGHT, HERO_SIZE_WIDTH } from '../../../../settings/constants';
 
+import { Container, ContainerArrow } from './styles';
 
 export const Arrows = () => {
-  const {left, bottom, setHeight, setWidth, setBottom, setLeft, setDirectionHero} = useContext(HeroMoveContext);
-  const [isArrowRightPress, setIsArrowRightPress] = useState (false)
-  const [isArrowLeftPress, setIsArrowLeftPress] = useState (false)
-  const [isArrowUpPress, setIsArrowUpPress] = useState (false)
-  const [isArrowSpacePress, setIsArrowSpacePress] = useState (false)
-  const GRAVITY = 0.9
-
-  const timerId = useRef<any>()
-
-  useEffect(() => {
-    if (isArrowRightPress){
-      rightMove()
-    } 
-    if (isArrowLeftPress){
-      leftMove()
-    } 
-    if (isArrowUpPress  || isArrowSpacePress){
-      upMove()
-    } 
-    if (bottom > 25) {
-      clearInterval(timerId.current)
-      timerId.current = undefined
-    }
-    if (bottom == 0) {
-      setBottom(0.000001)
-    }
-  }, [bottom])
-
-  const downMove = () => {
-    if (bottom > 0){
-      setBottom(bottom-1)
-    }
-    if (bottom < 0){
-      setBottom(0)
-    }
-  }
-
-  const upMove = async () => {
-    if (bottom === 0 && bottom < 15 && !timerId?.current) {
-      timerId.current = setInterval(() => { 
-        setBottom((b: number) => b+GRAVITY);
-      }, 10)
-    }
-  }
-
-  const spaceMove = () => {
-    if (bottom === 0 && bottom < 15) {
-      upMove()
-    }
-  }
-
-  const rightMove = () => {
-    if (left >= 0 && left < 90) {
-      setLeft(left+0.25)
-      setDirectionHero('RIGHT')
-    }
-  }
-
-  const leftMove = () => {
-    if (left > 0 && left <= 90){
-      setLeft(left-0.25)
-      setDirectionHero('LEFT')
-    }
-  }
-
-  useEventListener('keydown', (event: any) => {
-    if (event.key === 'ArrowLeft'){
-      setWidth(HERO_SIZE_WIDTH*0.85)
-      setIsArrowLeftPress(true)
-      leftMove()
-    } else if (event.key === 'ArrowRight'){
-      setWidth(HERO_SIZE_WIDTH*0.85)
-      setIsArrowRightPress(true)
-      rightMove()
-    } else if (event.key === 'ArrowDown'){
-      setHeight(HERO_SIZE_HEIGHT*0.75)
-      downMove()
-    } else if (event.key === 'ArrowUp'){
-      setIsArrowUpPress(true)
-      upMove()
-    } else if (event.key === ' '){
-      setIsArrowSpacePress(true)
-      spaceMove()
-    }
-  })
-
-  useEventListener('keyup', (event: any) => {
-    if (event.key === 'ArrowLeft'){
-      setWidth(HERO_SIZE_WIDTH)
-      setIsArrowLeftPress(false)
-    } else if (event.key === 'ArrowRight'){
-      setWidth(HERO_SIZE_WIDTH)
-      setIsArrowRightPress(false)
-    } else if (event.key === 'ArrowDown'){
-      setHeight(HERO_SIZE_HEIGHT)
-    } else if (event.key === 'ArrowUp'){
-      setIsArrowUpPress(false)
-    } else if (event.key === ' '){
-      setIsArrowSpacePress(false)
-    }
-  })
-
   return (
     <Container>
       <ContainerArrow>
--- src/context/moveHeroContext.tsx
+++ src/context/moveHeroContext.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent, ReactElement, useEffect, useState,  } from "react";
+import React, { FunctionComponent, ReactElement, useEffect, useRef, useState,  } from "react";
 import { HERO_SIZE_HEIGHT, HERO_SIZE_WIDTH } from "../settings/constants";
+import useEventListener from '@use-it/event-listener';
 
 interface IProps {
   children: ReactElement;
@@ -11,13 +12,7 @@ interface IHeroMoveContext {
   directionHero: string;
   height: number;
   width: number;
-  setBottom: (bottom: any) => void;
-  setLeft: (left: any) => void;
-  setDirectionHero: (directionHero: string) => void;
-  setHeight: (height: number) => void;
-  setWidth: (width: number) => void;
 }
-
 
 export const HeroMoveContext = React.createContext({} as IHeroMoveContext);
 
@@ -27,12 +22,34 @@ export const MoveHeroContextProvider: FunctionComponent<IProps> = ({children}) =
   const [width, setWidth] = useState(HERO_SIZE_WIDTH);
   const [height, setHeight] = useState(HERO_SIZE_HEIGHT);
   const [directionHero, setDirectionHero] = useState('RIGHT');
-  const gravity = 0.9
-
+  const [isArrowRightPress, setIsArrowRightPress] = useState (false)
+  const [isArrowLeftPress, setIsArrowLeftPress] = useState (false)
+  const [isArrowUpPress, setIsArrowUpPress] = useState (false)
+  const [isArrowSpacePress, setIsArrowSpacePress] = useState (false)
+  const timerId = useRef<any>()
+  const GRAVITY = 0.9
+    
   useEffect(() => {
+    if (isArrowRightPress){
+      rightMove()
+    } 
+    if (isArrowLeftPress){
+      leftMove()
+    } 
+    if (isArrowUpPress  || isArrowSpacePress){
+      upMove()
+    } 
+    if (bottom > 25) {
+      clearInterval(timerId.current)
+      timerId.current = undefined
+    }
+    // This logic is made because useEffect stop working when bottom is 0 and the Hero can't stay pressed the button for left or right.
+    if (bottom === 0) {
+      setBottom(0.000001)
+    }
     if (bottom > 0){
       const timeId = setInterval(() => {   
-        setBottom(bottom-gravity);
+        setBottom(bottom-GRAVITY);
       }, 20)
       return () => {
         clearInterval(timeId)
@@ -41,12 +58,83 @@ export const MoveHeroContextProvider: FunctionComponent<IProps> = ({children}) =
       setBottom(0)
     }
   }, [bottom])
-  
+
+  const downMove = () => {
+    if (bottom > 0){
+      setBottom(bottom-1)
+    }
+    if (bottom < 0){
+      setBottom(0)
+    }
+  }
+
+  const upMove = async () => {
+    if (bottom === 0 && bottom < 15 && !timerId?.current) {
+      timerId.current = setInterval(() => { 
+        setBottom((b: number) => b+GRAVITY);
+      }, 10)
+    }
+  }
+
+  const spaceMove = () => {
+    if (bottom === 0 && bottom < 15) {
+      upMove()
+    }
+  }
+
+  const rightMove = () => {
+    if (left >= 0 && left < 90) {
+      setLeft(left+0.25)
+      setDirectionHero('RIGHT')
+    }
+  }
+
+  const leftMove = () => {
+    if (left > 0 && left <= 90){
+      setLeft(left-0.25)
+      setDirectionHero('LEFT')
+    }
+  }
+
+  useEventListener('keydown', (event: any) => {
+    if (event.key === 'ArrowLeft'){
+      setWidth(HERO_SIZE_WIDTH*0.85)
+      setIsArrowLeftPress(true)
+      leftMove()
+    } else if (event.key === 'ArrowRight'){
+      setWidth(HERO_SIZE_WIDTH*0.85)
+      setIsArrowRightPress(true)
+      rightMove()
+    } else if (event.key === 'ArrowDown'){
+      setHeight(HERO_SIZE_HEIGHT*0.75)
+      downMove()
+    } else if (event.key === 'ArrowUp'){
+      setIsArrowUpPress(true)
+      upMove()
+    } else if (event.key === ' '){
+      setIsArrowSpacePress(true)
+      spaceMove()
+    }
+  })
+
+  useEventListener('keyup', (event: any) => {
+    if (event.key === 'ArrowLeft'){
+      setWidth(HERO_SIZE_WIDTH)
+      setIsArrowLeftPress(false)
+    } else if (event.key === 'ArrowRight'){
+      setWidth(HERO_SIZE_WIDTH)
+      setIsArrowRightPress(false)
+    } else if (event.key === 'ArrowDown'){
+      setHeight(HERO_SIZE_HEIGHT)
+    } else if (event.key === 'ArrowUp'){
+      setIsArrowUpPress(false)
+    } else if (event.key === ' '){
+      setIsArrowSpacePress(false)
+    }
+  })
 
   return (
-    <HeroMoveContext.Provider value={{
-      bottom, left, directionHero, width, height, 
-      setDirectionHero, setBottom, setLeft, setHeight, setWidth}}>
+    <HeroMoveContext.Provider value={{bottom, left, directionHero, width, height }}>
       {children}
     </HeroMoveContext.Provider>
   );
