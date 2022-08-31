e0172b70ecccd1aa0148022ba62db4b8c5ef5e8d
Cassio <cassioate@gmail.com>
2022-08-31 00:43:32 -0300

    feat: hero only can stay in the platform, or him go to die

--- src/components/molecules/platforms/index.tsx
+++ src/components/molecules/platforms/index.tsx
@@ -2,8 +2,7 @@
 import React, { useContext, useEffect, useRef } from "react";
 import { GameContext } from "../../../context/gameContext";
 import { HeroMoveContext } from "../../../context/heroPropsContext";
-import { KeyboardContext } from "../../../context/keyboardContext";
-import { HERO_SIZE_WIDTH_HIT_BOX, POSITION_Y_TO_DIE, START_FLOOR, START_MAX_JUMP, START_POSITION } from "../../../settings/constants";
+import { HERO_SIZE_WIDTH_HIT_BOX, POSITION_Y_TO_DIE, START_FLOOR, START_MAX_JUMP } from "../../../settings/constants";
 import { Platform } from "../../atoms/platform";
 
 interface Platform {
@@ -20,37 +19,37 @@ export const Platforms = () => {
 
   const FIRST_PLATFORM_X_Y: Platform = {x: 0, y:0, width: 150, height: 30}
   const SECOND_PLATFORM_X_Y: Platform  = {x: 200, y:50, width: 150, height: 30}
-  // const THIRD_PLATFORM_Y_X = [5, 150]
-  // const FOURTH_PLATFORM_Y_X = [5, 150]
+  const THIRD_PLATFORM_Y_X: Platform  = {x: 400, y:100, width: 150, height: 30}
+  const FOURTH_PLATFORM_Y_X: Platform  = {x: 700, y:250, width: 150, height: 30}
+
 
   const platforms: Platform[] = [
     FIRST_PLATFORM_X_Y,
-    SECOND_PLATFORM_X_Y
+    SECOND_PLATFORM_X_Y,
+    THIRD_PLATFORM_Y_X,
+    FOURTH_PLATFORM_Y_X
   ]
   
+  const validatorPlatform: Platform[] = []
   useEffect(() => {
-    let validatorPlatform: Platform[] = []
     platforms.forEach((platform) => {
       if (
         POSITION_X + HERO_SIZE_WIDTH_HIT_BOX >= platform.x && 
         POSITION_X <= platform.x + platform.width &&
         POSITION_Y <= platform.y + platform.height 
         ) {
-        // console.log(platform.y+platform.height)
-        console.log(platform.y + platform.height, POSITION_Y, 'Y')
-        console.log(platform.x + platform.width, POSITION_X, 'X')
         validatorPlatform.push(platform)
       }
     })
     if (validatorPlatform.length > 0){
-      setMAX_JUMP(validatorPlatform[0].y + START_MAX_JUMP)
+      setMAX_JUMP(validatorPlatform[0].height + START_MAX_JUMP)
       setIN_PLATFORM(true)
       setFLOOR(validatorPlatform[0].y + validatorPlatform[0].height)
     } else {
       setIN_PLATFORM(false)
       setFLOOR(START_FLOOR)
     }
-    console.log(IN_PLATFORM)
+    console.log(validatorPlatform)
   }, [POSITION_X, POSITION_Y])
 
   useEffect(() => {
@@ -73,6 +72,20 @@ export const Platforms = () => {
         left={SECOND_PLATFORM_X_Y.x}
         width={SECOND_PLATFORM_X_Y.width}
         height={SECOND_PLATFORM_X_Y.height}
+        img={'./assets/platforms/platform1.png'}
+      />
+      <Platform
+        bottom={THIRD_PLATFORM_Y_X.y}
+        left={THIRD_PLATFORM_Y_X.x}
+        width={THIRD_PLATFORM_Y_X.width}
+        height={THIRD_PLATFORM_Y_X.height}
+        img={'./assets/platforms/platform1.png'}
+      />
+      <Platform
+        bottom={FOURTH_PLATFORM_Y_X.y}
+        left={FOURTH_PLATFORM_Y_X.x}
+        width={FOURTH_PLATFORM_Y_X.width}
+        height={FOURTH_PLATFORM_Y_X.height}
         img={'./assets/platforms/platform1.png'}
       />
     </>
